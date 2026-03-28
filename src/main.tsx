3dd84da205ea643a90787c0bdef4f48933c4aea7
Boabyr <rolandpantucek@gmail.com>
2026-03-28 17:47:46 +0100

    feat: project structure, supabase client, routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>Dashboard kommt noch</div>} />
+        <Route path='/login' element={<div>Login kommt noch</div>} />
+        <Route path='/course/:courseId' element={<div>Kurs kommt noch</div>} />
+        <Route path='/course/:courseId/:topicId' element={<div>Thema kommt noch</div>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
